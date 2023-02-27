53e7575367354126617095152278cc1fb75104bb
jianzeping <jianzeping@baidu.com>
2023-02-28 00:14:44 +0800

    first commit

---
+++ cmd.sh
@@ -0,0 +1,8 @@
+
+echo "# BlocksGit" >> README.md
+git init
+git add README.md cmd.sh
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Zeping-Jian/BlocksGit.git
+git push -u origin main
